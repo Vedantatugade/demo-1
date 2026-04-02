83b95e455c643df96f854746936642870da1b361
Vedantatugade <vedantatugade@gmail.com>
2026-04-02 11:04:52 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,55 +1,50 @@
-variable "my_ip" {
-  description = "public IP for SSH"
+########################################
+# VPC & SUBNET VARIABLES
+########################################
+
+variable "vpc_id" {
+  description = "ID of the existing VPC"
   type        = string
 }
 
+variable "public_subnet_id" {
+  description = "ID of the existing public subnet"
+  type        = string
+}
+
+variable "private_subnet_id" {
+  description = "ID of the existing private subnet"
+  type        = string
+}
+
+
+variable "web_sg_id" {
+  description = "Security Group ID for Web Tier (existing)"
+  type        = string
+}
+
+variable "app_sg_id" {
+  description = "Security Group ID for App Tier (existing)"
+  type        = string
+}
+
+########################################
+# EC2 INSTANCE VARIABLES
+########################################
+
 variable "ami_id" {
-  description = "AMI ID"
+  description = "AMI ID for EC2 instances (must match region)"
   type        = string
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "Key pair name"
+  description = "Name of the EC2 Key Pair"
   type        = string
 }
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "public_subnet_id" {
-  description = "Public Subnet ID"
-  type        = string
-}
-
-variable "private_subnet_id" {
-  description = "Private Subnet ID"
-  type        = string
-}
-
-variable "internal_alb_sg_id" {
-  description = "internal lb sg"
-  type = string
-}
-
-variable "external_alb_sg_id" {
-  description = "internal lb sg"
-  type = string
-}
-
-variable "rdb_sg_id" {
-  description = "Database Security Group ID (manually created)"
-  type        = string
-}
-
-
-
-
 
