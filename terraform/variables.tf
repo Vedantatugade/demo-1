076d3b11216cd7203bb24eabd5f1c48d91e5de53
Vedantatugade <vedantatugade@gmail.com>
2026-04-02 11:59:21 +0530

    Refine variable descriptions in variables.tf
    Updated variable descriptions for clarity.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,3 @@
-########################################
-# VPC & SUBNET VARIABLES
-########################################
 
 variable "vpc_id" {
   description = "ID of the existing VPC"
@@ -8,43 +5,38 @@ variable "vpc_id" {
 }
 
 variable "public_subnet_id" {
-  description = "ID of the existing public subnet"
+  description = "ID of the public subnet"
   type        = string
 }
 
 variable "private_subnet_id" {
-  description = "ID of the existing private subnet"
+  description = "ID of the private subnet"
   type        = string
 }
 
 
 variable "web_sg_id" {
-  description = "Security Group ID for Web Tier (existing)"
+  description = "Web security group ID"
   type        = string
 }
 
 variable "app_sg_id" {
-  description = "Security Group ID for App Tier (existing)"
+  description = "App security group ID"
   type        = string
 }
 
-########################################
-# EC2 INSTANCE VARIABLES
-########################################
 
 variable "ami_id" {
-  description = "AMI ID for EC2 instances (must match region)"
+  description = "AMI ID"
   type        = string
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
 }
 
 variable "key_name" {
-  description = "Name of the EC2 Key Pair"
+  description = "Key pair name"
   type        = string
 }
-
