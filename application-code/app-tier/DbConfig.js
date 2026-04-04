f6c047898826afaeded128d3ac3d9570e5826249
vedant <vedantatugade@gmail.com>
2026-04-04 23:27:31 +0530

    remove sensitive db config

--- application-code/app-tier/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST : 'database-1.c8hq8ae00k97.us-east-1.rds.amazonaws.com',
-    DB_USER: "admin",
-    DB_PWD : 'vedantatugade',
-    DB_DATABASE : 'mydb'
-});
