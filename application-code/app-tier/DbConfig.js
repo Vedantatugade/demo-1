ab53b0fb6c5ce0d6998f4d102632658f0fdc164b
Vedantatugade <vedantatugade@gmail.com>
2026-06-27 13:27:23 +0530

    Update DbConfig.js with database configuration

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,1 +1,6 @@
-.
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.c8hq8ae00k97.us-east-1.rds.amazonaws.com',
+    DB_USER: "admin",
+    DB_PWD : 'Vedantatugade',
+    DB_DATABASE : 'webappdb'
+});
