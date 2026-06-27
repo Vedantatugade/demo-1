437e56fd64331318a21eccb941d1c77a6c604c4c
Vedantatugade <vedantatugade@gmail.com>
2026-06-27 13:13:57 +0530

    DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,1 @@
-module.exports = Object.freeze({
-    DB_HOST : 'database-1.c8hq8ae00k97.us-east-1.rds.amazonaws.com',
-    DB_USER: "admin",
-    DB_PWD : 'Vedantatugade',
-    DB_DATABASE : 'webappdb'
-});
+.
